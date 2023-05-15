0e0cf7b28bef22f3f65b6413da2e540b484e7f32
Ricki Heicklen <ricki.heicklen@gmail.com>
2023-05-15 15:59:32 -0400

    using heroku server instead of local server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const WebSocket = require('ws');
 
-const server = new WebSocket.Server({ port: 3000 });
+const server = new WebSocket.Server({ noServer: true });
+// const server = new WebSocket.Server({ port: 3000 });
 
 server.on('connection', (socket) => {
   console.log('Client connected');
@@ -17,7 +18,8 @@ server.on('connection', (socket) => {
         // Make a move in the game
         server.clients.forEach((client) => {
           if (client.readyState === WebSocket.OPEN) {
-            client.send(m);
+            client.send(JSON.stringify(m));
+            // client.send(m);
           }
         });
         break;
@@ -29,5 +31,22 @@ server.on('connection', (socket) => {
 
   socket.on('close', () => {
     console.log('Client disconnected');
+  });
+});
+
+const express = require('express');
+const app = express();
+
+app.use((req, res) => {
+  res.send('Tic Tac Toe Server');
+});
+
+const httpServer = app.listen(process.env.PORT || 3000, () => {
+  console.log('Server started on port ' + (process.env.PORT || 3000));
+});
+
+httpServer.on('upgrade', (request, socket, head) => {
+  server.handleUpgrade(request, socket, head, (socket) => {
+    server.emit('connection', socket, request);
   });
 });
